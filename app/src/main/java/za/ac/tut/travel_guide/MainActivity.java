23b8de5171a6dd338d48b3e18e7afe30c357d316
godfrey Timba <gntimba@gmail.com>
2019-01-24 16:43:34 +0200

    added persistent login

--- app/src/main/java/za/ac/tut/travel_guide/MainActivity.java
+++ app/src/main/java/za/ac/tut/travel_guide/MainActivity.java
@@ -59,7 +59,17 @@ public class MainActivity extends AppCompatActivity {
         //txtEmail=(TextView)findViewById(R.id.txtEmail);
 
     }
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_HOME);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);//***Change Here***
+        startActivity(intent);
+        finish();
+        System.exit(0);
 
+    }
 
 
     public void goToMainActivity(String email){
