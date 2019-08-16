feb2b732b60b759e64906e566bd384709e5992a0
godfrey Timba <gntimba@gmail.com>
2019-08-16 16:07:33 +0200

    chan

---
+++ app/src/main/gen/za/ac/tut/travel_guide/R.java
@@ -0,0 +1,7 @@
+/*___Generated_by_IDEA___*/
+
+package za.ac.tut.travel_guide;
+
+/* This stub is only used by the IDE. It is NOT the R class actually packed into the APK */
+public final class R {
+}
